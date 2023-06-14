da5513767056399623774a247f8a0c53b89ce5fb
SaharGGJ <124356732+SaharGGJ@users.noreply.github.com>
2023-06-14 18:06:36 +0300

    Basic Shapes

--- getStickbuggedlol/Assets/Scripts/GameManager.cs
+++ getStickbuggedlol/Assets/Scripts/GameManager.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject playerCharacter;
+    [SerializeField] GameObject boot;
     // Update is called once per frame
     void Update()
     {
